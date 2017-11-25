4a05722a3684db8a119e8eb9120d8be64c73894b
katsu1110 <code1110g-show@hotmail.co.jp>
2017-11-25 23:51:56 +0100

    dictionary

--- watching.py
+++ watching.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-dic2017_autumn = {'LivAndMaddie':15, 'juuni':7, 'fateapo':7, 'shokugeki':7}
+dic2017_autumn = {'LivAndMaddie':15, 'juuni':7, 'fateapo':7, 'shokugeki':7, 'rwby':6}
 
 def watching(dic, key):
     print('You are currently watching episode ' + str(dic[key]))
