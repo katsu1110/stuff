e2c48cb9a63e2ab20e57fa71f73cad4e659fb2be
katsu1110 <code1110g-show@hotmail.co.jp>
2017-12-16 20:44:14 +0100

    allerdings

--- vokabtest.py
+++ vokabtest.py
@@ -19,7 +19,7 @@ class Vokab:
                          'retten','Das Opfer','großartig','erschaffen','entführen',
                          'spinn nicht rum','beschwören','Der Neid','beneiden',
                          'auslösen','aufgeben','Der Schaden','Es hat sich erledigt',
-                         'gemütlich','aufsetzen','begreifen']
+                         'gemütlich','aufsetzen','begreifen','allerdings']
         
     def entfernen(self, w):
         self.wortlist.remove(w)
