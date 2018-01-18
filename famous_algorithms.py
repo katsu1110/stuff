c1c9aeb2c32232c44ce6ca34c08cb40e3d07cef2
katsu1110 <code1110g-show@hotmail.co.jp>
2018-01-18 23:01:29 +0100

    fizzbuzz

--- famous_algorithms.py
+++ famous_algorithms.py
@@ -5,6 +5,20 @@ Created on Tue Jan 16 22:30:32 2018
 @author: katsuhisa
 """
 
+#%%
+# FizzBuzz
+def fizzbuzz():
+    for i in range(1,101,1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+
+#%%
 # essential algorithm questions  (https://www.toptal.com/algorithms/interview-questions)
 '''How would you optimally calculate p^k, where k is non-negative integer? 
 What is the complexity of the solution?'''
