285ff8cc42da64c6da3698c4b41b2d45608c5c70
katsu1110 <code1110g-show@hotmail.co.jp>
2018-01-19 22:55:25 +0100

    insertion sort

--- famous_algorithms.py
+++ famous_algorithms.py
@@ -43,3 +43,14 @@ def bubble_sort(seq):
                 seq[i], seq[i+1] = seq[i+1], seq[i]
                 changed = True
     return seq
+
+# insertion sort (O(n^2))
+def insertion_sort(seq):
+    for i in range(1,len(seq)):
+        j = i - 1
+        key = seq[i]
+        while (seq[j] > key) and (j >= 0):
+            seq[j+1] = seq[j]
+            j -= 1
+        seq[j+1] = key
+    return seq
