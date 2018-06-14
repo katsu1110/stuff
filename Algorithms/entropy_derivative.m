17302d2646e7c6bf8b4c0c3790bbedcb73070e4c
katsu1110 <code1110g-show@hotmail.co.jp>
2018-06-14 07:59:24 +0200

    bug for derivative

--- Algorithms/entropy_derivative.m
+++ Algorithms/entropy_derivative.m
@@ -4,7 +4,8 @@ function entropy_derivative
 p = 0.01:0.01:0.99;
 e = @(p)-(1-p).*log2(1-p) - p.*log2(p);
 plot(p, e(p))
-d = @(p)log2((1-p)./p) - 2/log(2);
+d = @(p)log2((1-p)./p);
 hold on;
+d(0.5)
 plot(p, d(p))
 legend('entropy', 'derivative', 'boxoff')
